3763bc103c5e4a4467e2374316aee436e068fb63
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-18 21:25:12 +1000

    style: not sticky header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,11 @@
 import { Logo } from "@/components/logo"
-import { cn } from "@/utils/classname"
 import React from "react"
 
 export interface Props extends React.ComponentProps<"header"> {}
 
-export function Header({ className, ...props }: Props) {
+export function Header(props: Props) {
 	return (
-		<header className={cn("backdrop-blur-safe sticky top-0", className)} {...props}>
+		<header {...props}>
 			<div className="mx-auto flex max-w-3xl items-center justify-between px-8 py-6 xl:max-w-4xl">
 				<Logo />
 				<div className="flex items-center gap-8">
