7d538d1362eb65282cc4fb4f3791d789f12d81c7
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-19 16:03:58 +1000

    fix(media-player): audio not found

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	reactCompiler: true,
+	assetPrefix: "/components",
 }
 
 export default nextConfig
