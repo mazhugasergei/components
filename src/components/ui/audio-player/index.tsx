e8173422574ce9bea7ce5001f7a8c628c3b0d66f
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-20 12:13:05 +1000

    fix(audio-player): mustic naming

--- src/components/ui/audio-player/index.tsx
+++ src/components/ui/audio-player/index.tsx
@@ -6,9 +6,9 @@ import { ComponentProps, useCallback, useEffect, useRef, useState } from "react"
 import { ListIcon, NextIcon, PauseIcon, PlayIcon, PrevIcon } from "./icons"
 
 export const TRACKS = [
-	"audio/Elysium_Sound_-_Cosmic_Dreamer_-_Synthwave_Cyberpunk.mp3",
+	"audio/Elysium_Sound_-_Cosmic_Dreamer.mp3",
 	"audio/Greg_Kirkelie_-_1980s_Synthwave.mp3",
-	"audio/Elysium_Sound_-_Stellar_Sunset_Middle_-_Synth_Pop_Retro_Music.mp3",
+	"audio/Elysium_Sound_-_Stellar_Sunset_Middle.mp3",
 ]
 
 const formatTime = (t: number) => `${Math.floor(t / 60)}:${String(Math.floor(t % 60)).padStart(2, "0")}`
