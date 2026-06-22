a1537e019f767ed42de5705417416f551e952cbf
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-22 14:42:41 +1000

    refactor(audio-player): music path

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
 	reactCompiler: true,
-	output: "export",
 	basePath: "/components",
-	assetPrefix: "/components",
-	env: {
-		NEXT_PUBLIC_BASE_PATH: "/components",
-	},
 }
 
 export default nextConfig
